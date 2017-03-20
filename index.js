7edfa99a4dbc22e8f5d9242a2fd85cbc5199a7db
Matt Owens <omatt420@gmail.com>
2017-03-20 14:10:19 -0500

    include lodash

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// dependencies
+var _ = require('lodash');
+// data
 var androidEndpoints = require('./android.endpoints.js').data;
 var iosEndpoints = require('./ios.endpoints.js').data;
 var swagger = require('./swagger.json');
