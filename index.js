5e84f84ba11a231cc0c16d8ee3facac3d1ae6cf8
Matt Owens <omatt420@gmail.com>
2017-03-22 13:52:41 -0500

    parsing paramsgit add -A

--- index.js
+++ index.js
@@ -12,46 +12,109 @@ var mobileEndpoints = _.intersectionWith(androidEndpoints, iosEndpoints, _.isEqu
 
 var results = [];
 
-function getModels(input) {
-	
-}
-
+var testEndpoint = {};
 _.forEach(swagger.paths, function(obj, key) {
-	_.forEach(mobileEndpoints, function(me) {
-		if (key == me.path) {
-			_.forEach(obj, function(o, method) {
-				if (method == me.method.toLowerCase()) {
-
-					_.forEach(o.responses, function(r) {
-						if (r.schema.$ref) {
-							// gets top level response object
-							r.schema.$ref = _.pick(swagger.definitions, r.schema.$ref);
-							for (var i in r.schema.$ref) {
-								console.log(r.schema.$ref[i].properties);
-
-							}
-							_.forEach(r.schema.$ref.properties);
-						}
-					});
-					//console.log(JSON.stringify(o.responses));
-
-					// fill in submodels
-					var match = {
-						path: me.path,
-						method: me.method,
-						parameters: o.parameters,
-						response: o.responses
-					};
-					results.push(match);
-				}
-			});
-		}
-	});
+	if (key == '/v1/Places/{placeId}/Info') {
+		_.forEach(obj, function(o, method) {
+			if (method == 'put') {
+				o.parameters = parseParams(o.parameters);
+				console.log(o.parameters);
+				var result = {
+					path : 'v1/Places/{placeId}/Info',
+					method: 'PUT',
+					params: o.parameters
+				};
+				results.push(result);
+			}
+		});
+	}
 });
 
-// var csv = json2csv({ data: results });
 
-// fs.writeFile('file.csv', csv, function(err) {
-//   if (err) throw err;
-//   console.log('file saved');
+
+function parseParams(params) {
+	q = [];
+	_.forEach(params, function(p) {
+		if (p.schema) {
+			var parameter = {
+				    name: p.name,
+				    in: p.in,
+				    description: p.description,
+				    required: p.required,
+				    type: p.type,
+				    format: p.format,
+				    schema: parseObject(p.schema.$ref)
+				};
+				q.push(parameter);
+			}
+		else {
+			q.push(p);
+		}
+	});
+	return q;
+}
+
+// function parseResponse(resp) {
+
+// }
+
+function parseObject(obj) {
+	var r = {};
+	var model = _.pick(swagger.definitions, obj);
+	var modelKey = Object.keys(model);
+
+	if (model[modelKey].properties) {
+		_.forEach(model[modelKey].properties, function(p, propertyKey) {
+			if (p.items) {
+				r[propertyKey] = parseObject(p.items.$ref);
+			}
+			else if (p.$ref) {
+				r[propertyKey] = parseObject(p.$ref);
+			}
+			else {
+				r[propertyKey] = p;
+			}
+		});
+		return r;
+	}
+}
+
+
+// _.forEach(swagger.paths, function(obj, key) {
+// 	_.forEach(mobileEndpoints, function(me) {
+// 		if (key == me.path) {
+// 			_.forEach(obj, function(o, method) {
+// 				if (method == me.method.toLowerCase()) {
+
+// 					_.forEach(o.responses, function(r) {
+// 						if (r.schema.$ref) {
+// 							// gets top level response object
+// 							r.schema.$ref = _.pick(swagger.definitions, r.schema.$ref);
+// 							for (var i in r.schema.$ref) {
+// 								_.forEach(r.schema.$ref[i], function(p) {
+// 								});
+// 							}
+// 						}
+// 					});
+// 					//console.log(JSON.stringify(o.responses));
+
+// 					// fill in submodels
+// 					var match = {
+// 						path: me.path,
+// 						method: me.method,
+// 						parameters: parseParams(o.parameters)
+// 						// response: parseResponse(o.responses)
+// 					};
+// 					results.push(match);
+// 				}
+// 			});
+// 		}
+// 	});
 // });
+
+var csv = json2csv({ data: results });
+
+fs.writeFile('file.csv', csv, function(err) {
+  if (err) throw err;
+  console.log('file saved');
+});
